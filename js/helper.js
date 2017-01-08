e7f7a65d93d55b35fdb5909d34d4201d788dfae0
Ilyas F <it83.mohammed@gmail.com>
2017-01-08 18:21:49 +0530

    Updates

---
+++ js/helper.js
@@ -0,0 +1,21 @@
+﻿jQuery.getMultipleJSON = function () {
+    var
+      num = 0,
+      def = jQuery.Deferred(),
+      map = jQuery.map(arguments, function (jsonfile) {
+          return jQuery.getJSON(jsonfile).then(function () {
+              def.notify(1 / map.length * ++num, num, map.length);
+              return arguments;
+          });
+      })
+    ;
+    jQuery.when.apply(jQuery, map)
+      .fail(function () { def.rejectWith(def, arguments); })
+      .done(function () {
+          def.resolveWith(def, jQuery.map(arguments, function (response) {
+              return response[0];
+          }));
+      })
+    ;
+    return def;
+};
